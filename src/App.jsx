6e5ec11e5180586369598fc40067469fe8c179f8
rkulur <rashwithkulur07@gmail.com>
2024-12-20 22:36:12 +0530

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -7,23 +7,30 @@ import ModalProvider from "./context/ModalProvider";
 import EmployeeProvider from "./context/EmployeeProvider";
 
 function App() {
-	return (
-		<div className=" flex flex-col h-screen items-center justify-center bg-slate-50">
-			<StudentProvider>
-				<ModalProvider>
-					<EmployeeProvider>
-						<Router>
-							<Routes>
-								<Route path="/" element={<Veiw />}></Route>
-								<Route exact path="/insert" element={<Insert />}></Route>
-								<Route exact path="/edit/:id" element={<Edit />}></Route>
-							</Routes>
-						</Router>
-					</EmployeeProvider>
-				</ModalProvider>
-			</StudentProvider>
-		</div>
-	);
+  return (
+    <div className=" flex flex-col h-screen items-center justify-center bg-slate-50">
+      <StudentProvider>
+        <ModalProvider>
+          <EmployeeProvider>
+            <Router>
+              <Routes>
+                <Route
+                  path="/"
+                  element={
+                    <>
+                      <Veiw />
+                    </>
+                  }
+                ></Route>
+                <Route exact path="/insert" element={<Insert />}></Route>
+                <Route exact path="/edit/:id" element={<Edit />}></Route>
+              </Routes>
+            </Router>
+          </EmployeeProvider>
+        </ModalProvider>
+      </StudentProvider>
+    </div>
+  );
 }
 
 export default App;
--- src/components/Veiw.jsx
+++ src/components/Veiw.jsx
@@ -7,121 +7,113 @@ import Modal from "./Modal";
 import { useEmployees } from "../context/EmployeeProvider";
 
 export default function Veiw() {
-	const { setName, setPhone, setEmail, setAddress, setId } = useContext(StudentContext);
+  const { setName, setPhone, setEmail, setAddress, setId } =
+    useContext(StudentContext);
 
-	const { isConfirmed, setIsConfirmed } = useContext(ModalContext);
-	const { employees, setEmployees } = useEmployees();
+  const { isConfirmed, setIsConfirmed } = useContext(ModalContext);
+  const { employees, setEmployees } = useEmployees();
 
-	const [students, setStudents] = useState([]);
-	const [isDeleted, setIsDeleted] = useState(false);
-	const [isDeleteClicked, setIsDeleteClicked] = useState(false);
+  const [isDeleted, setIsDeleted] = useState(false);
+  const [isDeleteClicked, setIsDeleteClicked] = useState(false);
 
-	const btnRef = useRef(null)
+  const btnRef = useRef(null);
 
-	useEffect(() => {
-		if (isDeleted) {
-			console.log("true")
-			setIsDeleted(false);
-		}
-		if(isConfirmed){
-			setIsConfirmed(false)
-			handleDelete(btnRef)
-		}
-		setEmployees(JSON.parse(localStorage.getItem("employees")));
-	}, [isDeleted,isConfirmed]);
+  useEffect(() => {
+    if (isDeleted) {
+      console.log("true");
+      setIsDeleted(false);
+    }
+    if (isConfirmed) {
+      setIsConfirmed(false);
+      handleDelete(btnRef);
+    }
+    setEmployees(JSON.parse(localStorage.getItem("employees")));
+  }, [isDeleted, isConfirmed]);
 
-	
-	function handleClearInput() {
-		setName("");
-		setPhone("");
-		setEmail("");
-		setAddress("");
-	}
+  function handleClearInput() {
+    setName("");
+    setPhone("");
+    setEmail("");
+    setAddress("");
+  }
 
-	function handleDelete(e) {
-		const otherEmps = employees.filter((emp) => emp.id != e.current.id);
-		
-		localStorage.setItem("employees", JSON.stringify(otherEmps));
-		
-		setEmployees(otherEmps);
-		setIsDeleted(true);
-	}
+  function handleDelete(e) {
+    const otherEmps = employees.filter((emp) => emp.id != e.current.id);
 
-	return (
-		<>
-			{isDeleteClicked && <Modal setIsDeleteClicked={setIsDeleteClicked} />}
-			<div className={`${isDeleteClicked ? "blur-sm" : ""}`}>
-				<Link to={"/insert"} onClick={handleClearInput} className="w-12 h-12 border">
-					<div className="flex items-center justify-center">
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							fill="none"
-							viewBox="0 0 24 24"
-							strokeWidth={1.5}
-							stroke="currentColor"
-							className="w-12 h-12 rounded-full active:scale-[.9] shadow-sm bg-green-200 flex  items-center p-2 justify-center mb-6 "
-						>
-							<path
-								strokeLinecap="round"
-								strokeLinejoin="round"
-								d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
-							/>
-						</svg>
-					</div>
-				</Link>
-				{employees?.length > 0 ? (
-					<table className="veiw">
-						<thead>
-							<tr>
-								<th>Id</th>
-								<th>Name</th>
-								<th>Phone No</th>
-								<th>Email</th>
-								<th>Address</th>
-								<th>Edit</th>
-								<th>Delete</th>
-							</tr>
-						</thead>
-						<tbody>
-							{employees?.map((employee, index) => {
-								return (
-									<tr key={employee.id}>
-										<td>{employee.id}</td>
-										<td>{employee.name}</td>
-										<td>{employee.phone}</td>
-										<td>{employee.email}</td>
-										<td>{employee.address}</td>
-										<td>
-											<Link to={`/edit/${employee.id}`}>
-												<button
-													name={index}
-													className="px-4 py-2 bg-slate-200 rounded-lg active:scale-[.9]"
-												>
-													Edit
-												</button>
-											</Link>
-										</td>
-										<td>
-											<button
-											ref={btnRef}
-												id={employee.id}
-												onClick={()=>{
-													setIsDeleteClicked((prev) => !prev);
-												}}
-												className="px-4 py-2 bg-red-400 rounded-lg active:scale-[.9]"
-											>
-												Delete
-											</button>
-										</td>
-									</tr>
-								);
-							})}
-						</tbody>
-					</table>
-				) : (
-					<h1> No data to show </h1>
-				)}
-			</div>
-		</>
-	);
+    localStorage.setItem("employees", JSON.stringify(otherEmps));
+
+    setEmployees(otherEmps);
+    setIsDeleted(true);
+  }
+
+  return (
+    <>
+      {isDeleteClicked && <Modal setIsDeleteClicked={setIsDeleteClicked} />}
+      <div className={`${isDeleteClicked ? "blur-sm" : ""}`}>
+        <Link
+          to={"/insert"}
+          onClick={handleClearInput}
+          className="w-12 h-12 border"
+        >
+          <div className="flex items-center justify-center">
+            <button className="px-4 py-2 bg-green-200 rounded-lg active:scale-[.9]">
+              Add Student
+            </button>
+          </div>
+        </Link>
+        {employees?.length > 0 ? (
+          <table className="veiw">
+            <thead>
+              <tr>
+                <th>Id</th>
+                <th>Name</th>
+                <th>Phone No</th>
+                <th>Email</th>
+                <th>Address</th>
+                <th>Edit</th>
+                <th>Delete</th>
+              </tr>
+            </thead>
+            <tbody>
+              {employees?.map((employee, index) => {
+                return (
+                  <tr key={employee.id}>
+                    <td>{employee.id}</td>
+                    <td>{employee.name}</td>
+                    <td>{employee.phone}</td>
+                    <td>{employee.email}</td>
+                    <td>{employee.address}</td>
+                    <td>
+                      <Link to={`/edit/${employee.id}`}>
+                        <button
+                          name={index}
+                          className="px-4 py-2 bg-slate-200 rounded-lg active:scale-[.9]"
+                        >
+                          Edit
+                        </button>
+                      </Link>
+                    </td>
+                    <td>
+                      <button
+                        ref={btnRef}
+                        id={employee.id}
+                        onClick={() => {
+                          setIsDeleteClicked((prev) => !prev);
+                        }}
+                        className="px-4 py-2 bg-red-400 rounded-lg active:scale-[.9]"
+                      >
+                        Delete
+                      </button>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        ) : (
+          <h1> No data to show </h1>
+        )}
+      </div>
+    </>
+  );
 }
